920056e3ac99bf7e4b9aee8855bd9e38f85e614a
Florian Janke <f.janke@tnnk.local>
2018-11-27 09:51:43 +0100

    Main menu einfügen Test

---
+++ game2.py
@@ -0,0 +1,6 @@
+#import both games (Penalty Shootout and Adventure)
+#import
+#import
+
+import import_spielername
+
---
+++ import_spielername.py
@@ -0,0 +1,4 @@
+def spielername():
+    spielername = input()
+    return input
+input("Welcome to the main Menu of Game2. Please state your name!\n")
