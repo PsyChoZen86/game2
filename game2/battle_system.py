24db4eaacb58bb436a2f61e800a1670934fd3e48
Alexander Schulze <a.schulze@tnnk.local>
2018-11-28 11:33:48 +0100

    boolean enemy_alive added which gets returned now as well as player hp

--- game2/battle_system.py
+++ game2/battle_system.py
@@ -21,6 +21,7 @@ def battle(player_hp, player_name):
     minimum = 8 #minimum damage value for player
     maximum = 18 #maximum damage value for player
     buff_duration = 99 #placeholder for buff counter
+    enemy_alive = True #indicator to know if enemy died or player fled
 
     #returns number of remaining HP as symbols to visualize it
     def hp_graphic(hp, sym):
@@ -94,7 +95,11 @@ def battle(player_hp, player_name):
             print ("\nPlease try again!\n")
         
     print ("The battle is over!\n")
+    
+    #check if enemy is still alive based on hp remaining
+    if enemy_hp <= 0:
+        enemy_alive = False
 
-    return player_hp
+    return player_hp, enemy_alive
 
 #battle(100, "Test")
