97bdb683df05bbad0e63d100ececa9b766775205
Alexander Schulze <a.schulze@tnnk.local>
2018-11-28 11:08:09 +0100

    added buff option which raises min and max atk for 3 turns

--- game2/battle_system.py
+++ game2/battle_system.py
@@ -1,19 +1,26 @@
 #a simple turn based battle system with randomly generated damage
 #and remaining enemy hit points display.
 #future versions should include: more battle options
-    
 
 
-def battle(player_hp,player_name):
+def battle(player_hp, player_name):
     
     #module needed for random damage calculation
     import random
 
+    #for testing
+    #player_hp = 100
+    #player_name = "Test"
+
+    #variables
     enemy_hp = 50 #indicator for remaining enemy hitpoints
     hp_symbol = "*" #symbol used for visualisation
     battle_active = True #indicator for battle loop (while battle is active)
     player_choice = "o" #placeholder for player choice variable
     attack_power = 0 #stores the last attacks value
+    minimum = 8 #minimum damage value for player
+    maximum = 18 #maximum damage value for player
+    buff_duration = 99 #placeholder for buff counter
 
     #returns number of remaining HP as symbols to visualize it
     def hp_graphic(hp, sym):
@@ -22,6 +29,7 @@ def battle(player_hp,player_name):
     def choose():
         return ("""
         (a)ttack
+        (b)attlestance
         (r)un
 
     Choose your destiny: """)
@@ -44,16 +52,30 @@ def battle(player_hp,player_name):
         player_choice = input (choose())
         
         #attack -> calculate and print damage
-        if player_choice == "a":
-            attack_power = random.randint(8,18)
-            print ("\nYou did", attack_power, "damage.")
-            enemy_hp = enemy_hp - attack_power
-            attack_power = random.randint(1,10)
+        if player_choice == "a": 
+            if buff_duration < 3:
+                attack_power = random.randint(minimum + 2, maximum + 2)
+                print ("\nYou did", attack_power, "damage.")
+                enemy_hp = enemy_hp - attack_power
+                buff_duration + 1
+            else:
+                attack_power = random.randint(minimum, maximum)
+                print ("\nYou did", attack_power, "damage.")
+                enemy_hp = enemy_hp - attack_power
+            
+            attack_power = random.randint(1, 10)
+                
             if attack_power > 3:
                 player_hp = player_hp - attack_power
                 print ("\nThe Troll did", attack_power, "damage.")
             else:
                 print ("\nYou dodged the attack.")
+                
+        #battlestance -> 3 rounds
+        elif player_choice =="b":
+            buff_duration = 0
+            print("\nYour minimum and maximum damage raises by 2!")
+            
                 
         #run -> success -> print escape message & leave loop
         elif player_choice == "r":
@@ -64,6 +86,7 @@ def battle(player_hp,player_name):
                 attack_power = random.randint(1,10)
                 player_hp = player_hp - attack_power
                 print ("\nThe Troll did", attack_power, "damage.")
+                buff_duration + 1
                 
             
         #other command -> print error message
@@ -74,4 +97,4 @@ def battle(player_hp,player_name):
 
     return player_hp
 
-#battle(100, "Hans") #for testing purposes
+#battle(100, "Test")
