6a82fb464e745d309613d3b19faa69512aa52c4a
polaris202 <45334347+polaris202@users.noreply.github.com>
2018-11-30 22:07:36 +0100

    bugfixes

--- game2/adventure_game.py
+++ game2/adventure_game.py
@@ -11,8 +11,6 @@ import function_print_message
 # todo: look and map can be merged into one file!
 # todo: room description + status visited message from configfile
 #
-# Bug: If the player avoids the fight monster will be removed from
-# monster_list. If not, the fight starts again.
 #
 # movement:   move direction
 # look:       look direction
@@ -35,7 +33,8 @@ def game(game2_playername):
             print("pos: ", pos)
             print("room: ", room)
             print("heading: ", heading)
-            print("room_l=", str(room_l))
+            print("room_l: ", room_l)
+            print("player_hp: ", player_hp)
             print("###")
             
     ########################################
@@ -47,7 +46,7 @@ def game(game2_playername):
     ########################################
     # variables
 
-    princess = random.randint(6, len(list_r)) 
+    princess = random.randint(6, len(list_r))
 
     pos = 1                                    
     
@@ -55,7 +54,7 @@ def game(game2_playername):
     room_l = list_l[pos - 1]                                                    # load initial room from viewlist
 
     heading = ""
-    player_hp = 100
+    player_hp = 10
     monster_list = [3, 6, 8, 9, 11, 13, 15, 17, 19]                             # need random function
 
     name = game2_playername
@@ -64,7 +63,7 @@ def game(game2_playername):
     # defs
 
     def input_command():
-        a, b = input("Where do you want to do? \n(type:) 'move direction' or 'look direction' 'north, south, east, west: ").split()
+        a, b = input("-" * 82 + "\nWhat do you want to do? \n(type:) 'move direction' or 'look direction' 'north, south, east, west: ").split()
         return a, b
 
     def print_room():
@@ -74,6 +73,7 @@ def game(game2_playername):
         print("-" * 82 + "\nYou can't move to " + heading)
 
     def input_err():
+        print()
         m = "Input Error!"
         print("-" * 82 + "\n" + m.center(82))
         print("\nYou have to input two words. 'command' and 'direction'")
@@ -89,7 +89,7 @@ def game(game2_playername):
         print("\n" + function_print_message.print_textfield(text, pl_name, star, padding))
         print("Find the princess before she get killed by the monsters.\nDon't waste your time.")
 
-        m = "Move!"
+        m = "\nMove!"
         print(m.center(82))
         
     def print_enemy_killed(h):
@@ -99,10 +99,16 @@ def game(game2_playername):
         print("-" * 82 + "\nYou didn't killed the enemy! Your health is", h, "HP")
 
     def print_found_princess():
-        print("-" * 82 + "\nYou are in room " + str(pos) + ". You have found the princess!")
+        m = "You have found the princess!"
+        print("-" * 82 + "\nYou are in room " + str(pos) + ".")
+        print()
+        print(m.center(82))
 
     def print_dead():
-        print("-" * 82 + "\nYou are dead!")
+        m = "You are dead!"
+        print()
+        print(m.center(82))
+
 
     ######################################
     # mainloop
@@ -116,7 +122,7 @@ def game(game2_playername):
             debug_1()
             print_room()                                                        # player position
             
-            while pos in monster_list and player_hp >= 0:          
+            if pos in monster_list and player_hp >= 0:
                 monster_pos = pos                                               # set monster pos to player pos
                 player_hp, monster_alive = battle_system.battle(player_hp, name)   # battle
                 if player_hp >= 0:
@@ -124,10 +130,8 @@ def game(game2_playername):
                         monster_list.remove(monster_pos)                        # delete monster from monster_list
                         print_enemy_killed(player_hp)
                     elif monster_alive is True:
-                        monster_list.remove(monster_pos)                    # Bug: if enemy is not removed > next battle
                         print_enemy_not_killed(player_hp)                       # enemy HP will be max on
-                elif player_hp <= 0:                                            # next room entry!!
-                    monster_list.remove(monster_pos)            
+                elif player_hp <= 0:                                            # next room entry!!            
                     print_dead()
                     
             command, subject = input_command()
@@ -137,7 +141,9 @@ def game(game2_playername):
                 if heading in room:
                     pos = int(room[heading])                                    # get next room pos from dict
                     room = list_r[pos - 1]                                      # switch room to pos
-                    room_l = list_l[pos - 1]                
+                    room_l = list_l[pos - 1]
+                    if player_hp <= 97:
+                        player_hp = player_hp + 3                               # adding HP to revive player
                 else:
                     move_err()
                     
@@ -145,10 +151,10 @@ def game(game2_playername):
                 heading = subject
                 if heading in room_l:
                     view = room_l[heading]                                       # get view text from roomlist
-                    print("-" * 82)
-                    print(view)
+                    print("-" * 82 + "\n" + view)
                 
         except ValueError:                                                       # catch if only one input
             input_err()
                         
     print_found_princess()
+
