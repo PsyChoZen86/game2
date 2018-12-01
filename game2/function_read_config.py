e23f45a678581ba1e8fb52a136edf63dbfe2c3dd
polaris202 <45334347+polaris202@users.noreply.github.com>
2018-12-01 16:26:49 +0100

    bugfixes

--- game2/function_read_config.py
+++ game2/function_read_config.py
@@ -20,20 +20,20 @@ def read_config(f):
     config_file = f
 
     with open(config_file, "r") as config_obj:
-        for line in config_obj:                                 # line is not used!!
-            list_1 = config_obj.read().splitlines()             # split at linebreak > write to list
-            list_2 = []
-            for i in list_1:                                    # for element in list: split > write to sublist
-                list_2.append(i.split("_"))
-                list_r = []
-            x = 0
-            while x < len(list_2):                              # for every index in list_2
-                dict_1 = {}
-                for block in list_2[x]:
-                    block = block.strip(",")                    # remove ,
-                    wort = block.split(":")                     # split at :
-                    dict_1[wort[0]] = wort[1]                   # make dict from direction : roomnumber as string                      
-                list_r.append(dict_1)                           # append to final list_r
-                x = x + 1                
+        data = config_obj.read()                            # read file to data
+        list_1 = data.splitlines()                          # split data to list_1
+        list_2 = []
+        for i in list_1:                                    # for element in list: split > write to sublist
+            list_2.append(i.split("_"))
+            list_r = []
+        x = 0
+        while x < len(list_2):                              # for every index in list_2
+            dict_1 = {}
+            for block in list_2[x]:
+                block = block.strip(",")                    # remove ,
+                word = block.split(":")                     # split at :
+                dict_1[word[0]] = word[1]                   # make dict from direction : roomnumber as string                      
+            list_r.append(dict_1)                           # append to final list_r
+            x = x + 1               
 
     return list_r
