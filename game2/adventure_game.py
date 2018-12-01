1ecf25812ea60df4f13c18bb2c7a2ef9dc70c423
polaris202 <45334347+polaris202@users.noreply.github.com>
2018-12-01 16:25:59 +0100

    final bugfixes and optimizations

--- game2/adventure_game.py
+++ game2/adventure_game.py
@@ -54,7 +54,7 @@ def game(game2_playername):
     room_l = list_l[pos - 1]                                                    # load initial room from viewlist
 
     heading = ""
-    player_hp = 10
+    player_hp = 100
     monster_list = [3, 6, 8, 9, 11, 13, 15, 17, 19]                             # need random function
 
     name = game2_playername
@@ -63,66 +63,71 @@ def game(game2_playername):
     # defs
 
     def input_command():
-        a, b = input("-" * 82 + "\nWhat do you want to do? \n(type:) 'move direction' or 'look direction' 'north, south, east, west: ").split()
+        a, b = input("-" * 82 + "\n\n\nWhat do you want to do? \n(type:) 'move direction' or 'look direction' 'north, south, east, west: ").split()
         return a, b
 
-    def print_room():
-        print("-" * 82 + "\nYou are in room " + str(pos) + ". No princess found.")
+    def print_room(h):
+        print("-" * 82 + "\n\n\nYou are in room " + str(pos) + ". No princess found. " + "Your health is", h, "HP")
 
     def move_err():
-        print("-" * 82 + "\nYou can't move to " + heading)
+        print("-" * 82 + "\n\n\nYou can't move to " + heading)
 
     def input_err():
         print()
         m = "Input Error!"
-        print("-" * 82 + "\n" + m.center(82))
+        print("-" * 82 + "\n\n\n" + m.center(82))
         print("\nYou have to input two words. 'command' and 'direction'")
         print("(type:) 'move north' or 'look east': ")
+        
+    def print_view():
+        print("-" * 82 + "\n\n\n" + view)
+        
+    def look_err():
+        print("-" * 82 + "\n\n\nYou can't look to " + heading)
 
     def print_intro(n):
         pl_name = n
         pl_name = pl_name + "!"
         text = "Welcome to Bloodshed Mansion"
         star = "+"
-        padding = 60
+        padding = 54
         print("-" * 82)
         print("\n" + function_print_message.print_textfield(text, pl_name, star, padding))
         print("Find the princess before she get killed by the monsters.\nDon't waste your time.")
 
-        m = "\nMove!"
-        print(m.center(82))
+        m = "Move!"
+        print("\n" + m.center(82))
         
     def print_enemy_killed(h):
-        print("-" * 82 + "\nYou have killed the enemy! Your health is", h, "HP")
+        print("-" * 82 + "\n\n\nYou have killed the enemy! Your health is", h, "HP")
 
     def print_enemy_not_killed(h):
-        print("-" * 82 + "\nYou didn't killed the enemy! Your health is", h, "HP")
+        print("-" * 82 + "\n\n\nYou didn't killed the enemy! Your health is", h, "HP")
 
     def print_found_princess():
         m = "You have found the princess!"
-        print("-" * 82 + "\nYou are in room " + str(pos) + ".")
+        print("-" * 82 + "\n\n\nYou are in room " + str(pos) + ".")
         print()
         print(m.center(82))
 
     def print_dead():
         m = "You are dead!"
         print()
-        print(m.center(82))
-
+        print("-" * 82 + "\n\n\n" + m.center(82))
 
     ######################################
     # mainloop
 
     debug_1()
     print_intro(name)
+    print_room(player_hp)
 
     while pos != princess:
         
         try:        
             debug_1()
-            print_room()                                                        # player position
             
-            if pos in monster_list and player_hp >= 0:
+            if pos in monster_list:
                 monster_pos = pos                                               # set monster pos to player pos
                 player_hp, monster_alive = battle_system.battle(player_hp, name)   # battle
                 if player_hp >= 0:
@@ -133,6 +138,7 @@ def game(game2_playername):
                         print_enemy_not_killed(player_hp)                       # enemy HP will be max on
                 elif player_hp <= 0:                                            # next room entry!!            
                     print_dead()
+                    quit()
                     
             command, subject = input_command()
 
@@ -142,6 +148,7 @@ def game(game2_playername):
                     pos = int(room[heading])                                    # get next room pos from dict
                     room = list_r[pos - 1]                                      # switch room to pos
                     room_l = list_l[pos - 1]
+                    print_room(player_hp)
                     if player_hp <= 97:
                         player_hp = player_hp + 3                               # adding HP to revive player
                 else:
@@ -151,10 +158,12 @@ def game(game2_playername):
                 heading = subject
                 if heading in room_l:
                     view = room_l[heading]                                       # get view text from roomlist
-                    print("-" * 82 + "\n" + view)
+                    print_view()
+                else:
+                    look_err()
                 
         except ValueError:                                                       # catch if only one input
             input_err()
                         
     print_found_princess()
-
+    quit()
